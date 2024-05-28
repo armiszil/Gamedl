8e2e777699f3939f7a6bc2fd3e64858c20f552e2
Gabor Szita <szitagabormatek@gmail.com>
2024-05-28 17:17:14 +0200

    initial commit

---
+++ Gamedl/src/main/java/com/project/gamedl/GamedlApplication.java
@@ -0,0 +1,13 @@
+package com.project.gamedl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedlApplication.class, args);
+	}
+
+}
