f414cefcdc3a796004b456f8f18567d0ff9ac02a
Gabor Szita <szitagabormatek@gmail.com>
2024-06-11 00:06:45 +0200

    changes the table schema along with the predefined data

--- GamedlAPI/docker/init.sql
+++ GamedlAPI/docker/init.sql
@@ -2,35 +2,34 @@ CREATE DATABASE IF NOT EXISTS gamedl_db;
 
 USE gamedl_db;
 
-CREATE TABLE IF NOT EXISTS Game (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS games (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     genre VARCHAR(255) NOT NULL,
-    franchiseName VARCHAR(255) NOT NULL,
-    releaseDate DATE NOT NULL
+    franchise_name VARCHAR(255) NOT NULL,
+    release_date DATE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Characters (
-    ID INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS characters (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL,
     gender VARCHAR(255) NOT NULL,
     age INT NOT NULL,
-    gameID INT NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (gameID) REFERENCES Game(ID) 
+    game_id_fk INT NOT NULL,
+    FOREIGN KEY (game_id_fk) REFERENCES games(id) 
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
 
-INSERT INTO Game (name, genre, franchiseName, releaseDate)
+INSERT INTO games (name, genre, franchise_name, release_date)
 VALUES
 ('The Legend of Zelda: Breath of the Wild', 'Action-adventure', 'The Legend of Zelda', '2017-03-03'),
 ('Super Mario Odyssey', 'Platform', 'Super Mario', '2017-10-27'),
 ('The Witcher 3: Wild Hunt', 'Role-playing', 'The Witcher', '2015-05-19');
 
-INSERT INTO Characters (name, role, gender, age, gameID)
+INSERT INTO characters (name, role, gender, age, game_id_fk)
 VALUES
 ('Link', 'Protagonist', 'Male', 17, 1),
 ('Zelda', 'Supporting', 'Female', 17, 1),
