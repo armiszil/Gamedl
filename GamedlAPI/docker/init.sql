a701b468076c2a361a0f4f895a6bd44379f444d6
Gabor Szita <szitagabormatek@gmail.com>
2024-06-14 13:55:58 +0200

    changes the init.sql to match the enum values

--- GamedlAPI/docker/init.sql
+++ GamedlAPI/docker/init.sql
@@ -25,18 +25,18 @@ CREATE TABLE IF NOT EXISTS characters (
 
 INSERT INTO games (name, genre, franchise_name, release_date)
 VALUES
-('The Legend of Zelda: Breath of the Wild', 'Action-adventure', 'The Legend of Zelda', '2017-03-03'),
-('Super Mario Odyssey', 'Platform', 'Super Mario', '2017-10-27'),
-('The Witcher 3: Wild Hunt', 'Role-playing', 'The Witcher', '2015-05-19');
+('The Legend of Zelda: Breath of the Wild', 'ACTIONADVENTURE', 'The Legend of Zelda', '2017-03-03'),
+('Super Mario Odyssey', 'PLATFORM', 'Super Mario', '2017-10-27'),
+('The Witcher 3: Wild Hunt', 'ROLEPLAYING', 'The Witcher', '2015-05-19');
 
 INSERT INTO characters (name, role, gender, age, game_id_fk)
 VALUES
-('Link', 'Protagonist', 'Male', 17, 1),
-('Zelda', 'Supporting', 'Female', 17, 1),
-('Mario', 'Protagonist', 'Male', 26, 2),
-('Princess Peach', 'Supporting', 'Female', 24, 2),
-('Geralt of Rivia', 'Protagonist', 'Male', 100, 3),
-('Ciri', 'Supporting', 'Female', 21, 3);
+('Link', 'PROTAGONIST', 'MALE', 17, 1),
+('Zelda', 'SUPPORTING', 'FEMALE', 17, 1),
+('Mario', 'PROTAGONIST', 'MALE', 26, 2),
+('Princess Peach', 'SUPPORTING', 'FEMALE', 24, 2),
+('Geralt of Rivia', 'PROTAGONIST', 'MALE', 100, 3),
+('Ciri', 'SUPPORTING', 'FEMALE', 21, 3);
 
 
 
