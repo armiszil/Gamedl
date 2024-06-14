c592432fb66c92326e7d137025e83c7d54e9f93a
Gabor Szita <szitagabormatek@gmail.com>
2024-06-14 14:37:53 +0200

    adds method in repository with a query to get

--- GamedlAPI/src/main/java/com/project/gamedl/data/CharacterRepository.java
+++ GamedlAPI/src/main/java/com/project/gamedl/data/CharacterRepository.java
@@ -2,10 +2,14 @@ package com.project.gamedl.data;
 
 import com.project.gamedl.domain.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<Character,Long> {
 
-    long count();
+    @Query(value = "SELECT * FROM characters ORDER BY rand() LIMIT 1", nativeQuery = true)
+    Optional<Character> getRandomCharacter();
 }
