dd25cbe8815953801c027031a37d96fa42879b04
Gabor Szita <szitagabormatek@gmail.com>
2024-06-19 11:41:23 +0200

    changes the Design elements folder name to design_elements to remove unnecessary spaces and capital letters

--- GamedlUI/gamedl-frontend/src/App.js
+++ GamedlUI/gamedl-frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import RandomCharacter from './RandomCharacter.js'; // Adjust the path if needed
-import Header from './Design elements/Header.js'
-import Banner from './Design elements/Banner.js'
-import Footer from './Design elements/Footer.js'
+import Header from './design_elements/Header.js'
+import Banner from './design_elements/Banner.js'
+import Footer from './design_elements/Footer.js'
 
 export default function App() {
     return (
